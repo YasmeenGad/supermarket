5bb1c2627cb1612ae1443d3d26f4f7142b18c4e9
Yasmeen Gad <gadyasmeen335@gmail.com>
2024-08-10 19:09:40 +0300

    create repo inreface of get favorite products feature

--- lib/features/favorite/domain/repositories/favorite_repo.dart
+++ lib/features/favorite/domain/repositories/favorite_repo.dart
@@ -1,6 +1,8 @@
 import 'package:dartz/dartz.dart';
 import 'package:supermarket/features/favorite/domain/entities/add_favorite.dart';
+import 'package:supermarket/features/favorite/domain/entities/get_favorite.dart';
 
 abstract class FavoriteRepository {
   Future<Either<String, AddFavorite>> addFavoriteProducts(List<String> productIds);
+  Future<GetFavorite> getFavoriteProducts(String id);
 }
