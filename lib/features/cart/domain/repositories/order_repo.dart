a4e0c629e847a193cb7c5d512a35795beaff05d1
Yasmeen Gad <gadyasmeen335@gmail.com>
2024-08-14 01:34:42 +0300

    creare repo interface and usecase of delete item

--- lib/features/cart/domain/repositories/order_repo.dart
+++ lib/features/cart/domain/repositories/order_repo.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:supermarket/features/cart/domain/entities/create_order_entity.dart';
+import 'package:supermarket/features/cart/domain/entities/delete_order.dart';
 import 'package:supermarket/features/cart/domain/entities/fetch_order_entities.dart';
 import 'package:supermarket/features/cart/domain/entities/get_total_order.dart';
 
@@ -11,5 +12,6 @@ abstract class OrderRepository {
   Future<Either<String, TotalOrder>> calculateOrderTotals(String orderId);
 
   Future<Either<String, MyOrder>> updateOrder(String orderId, List<String> productIds);
+  Future<Either<String, DeleteOrder>> deleteOrder(List<String> productIds);
 
 }
--- lib/features/cart/domain/usecases/order_usecase.dart
+++ lib/features/cart/domain/usecases/order_usecase.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:supermarket/features/cart/domain/entities/create_order_entity.dart';
+import 'package:supermarket/features/cart/domain/entities/delete_order.dart';
 import 'package:supermarket/features/cart/domain/entities/fetch_order_entities.dart';
 import 'package:supermarket/features/cart/domain/entities/get_total_order.dart';
 import 'package:supermarket/features/cart/domain/repositories/order_repo.dart';
@@ -7,6 +8,8 @@ import 'package:supermarket/features/cart/domain/repositories/order_repo.dart';
 typedef OrderResult = Either<String, MyOrder>;
 typedef FetchedOrderResult = Either<String, FetchedOrder>;
 typedef TotalOrderResult = Either<String, TotalOrder>;
+
+typedef DeleteOrderResult = Either<String, DeleteOrder>;
 
 class OrderUsecases {
   final OrderRepository repository;
@@ -27,5 +30,9 @@ class OrderUsecases {
 
   Future<OrderResult> updateOrder(String orderId, List<String> productIds) async {
     return await repository.updateOrder(orderId, productIds);
+  }
+
+  Future<DeleteOrderResult> deleteOrder(List<String> productIds) async {
+    return await repository.deleteOrder(productIds);
   }
 }
